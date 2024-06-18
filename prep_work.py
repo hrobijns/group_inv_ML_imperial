e1a8c8d9e9f4988f1700fa7fbc52b63536baf551
Hugo Robijns <155160680+hrobijns@users.noreply.github.com>
2024-06-18 01:00:49 +0100

    Update prep_work.py

--- prep_work.py
+++ prep_work.py
@@ -8,9 +8,10 @@ from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
 import urllib.request
 
+################################################################################
+#importing and wrangling data
 
 def data_wrangle(data_string):
-#loads in and cleans up the data used for the neural network
     file_path = f'{data_string}.txt'
     file_url = f'https://raw.githubusercontent.com/TomasSilva/MLcCY7/main/Data/{data_string}.txt'
     try:
@@ -23,9 +24,12 @@ def data_wrangle(data_string):
     data_list = ast.literal_eval(data_raw) #converts from string to list
     data_array = np.array(data_list) #converts from list to NumPy array
     return data_array
-    
+
+
+################################################################################
+#defining and training NN
+
 def get_network():
-#define the neural network
     inp = tf.keras.layers.Input(shape=(5,))
     prep = tf.keras.layers.Flatten()(inp)
     h1 = tf.keras.layers.Dense(1000, activation=tf.nn.relu)(prep)
@@ -41,7 +45,6 @@ def get_network():
     return model
 
 def train_network(X_train, y_train, X_test, y_test):
-#train the network
     model = get_network()
     early_stopping = EarlyStopping(monitor='val_loss', patience=3)
     history = model.fit(
@@ -52,9 +55,15 @@ def train_network(X_train, y_train, X_test, y_test):
     )
     return history
 
+################################################################################
+#run program
+
 if __name__ == '__main__':
-#run the program
     X = data_wrangle('WP4s')
     y = data_wrangle('WP4_Hodges')
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5) #split data into training and testing
     X_train, X_test = tf.keras.utils.normalize(X_train, axis=1), tf.keras.utils.normalize(X_test, axis=1) #normalise
+
+    model = get_network()    
+    print(model.summary()) #print an overview of the neural network created
+    history = train_network(X_train, y_train, X_test, y_test) #train network on chosen data
