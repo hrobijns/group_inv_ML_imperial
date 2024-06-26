fb9499ce9d325815360d681ab71238b7b09633d5
Hugo Robijns <155160680+hrobijns@users.noreply.github.com>
2024-06-27 00:02:44 +0100

    Update deep_sets.py

--- deep_sets.py
+++ deep_sets.py
@@ -1,1 +1,14 @@
-import tensorflow as tf 
+def get_network():
+    inp = tf.keras.layers.Input(shape=(5,))
+    prep = tf.keras.layers.Reshape((5,))(inp)
+    h1 = tf.keras.layers.Dense(16, activation='relu')(prep)
+    h2 = tf.keras.layers.Dense(32, activation='relu')(h1)
+    #adding a layer to the network which sums the inputs
+    summed = tf.keras.layers.Lambda(lambda x: K.sum(x, axis=1, keepdims=True))(h2)
+    out = tf.keras.layers.Dense(1, activation='linear')(summed)
+    model = tf.keras.models.Model(inputs=inp, outputs=out)
+    model.compile(
+        loss='mean_squared_error',
+        optimizer=tf.keras.optimizers.Adam(0.001)
+    )
+    return model
