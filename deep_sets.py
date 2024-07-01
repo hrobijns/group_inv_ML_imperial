b866c3c5016289574343562798296eb48e64a857
Hugo Robijns <155160680+hrobijns@users.noreply.github.com>
2024-07-01 16:02:32 +0100

    Update deep_sets.py

--- deep_sets.py
+++ deep_sets.py
@@ -1,12 +1,10 @@
-#import necessary libraries but also useful previously defined functions 
+# import necessary libraries but also useful previously defined functions 
 from SHodge_learn import data_wrangle_S
 from SHodge_learn import daattavya_accuracy
 from SHodge_learn import train_network
 
-from keras import layers, models, optimizers
-from sklearn.model_selection import train_test_split
-import numpy as np
 import requests
+import numpy as np
 import ast
 import tensorflow as tf
 from tensorflow.keras.callbacks import EarlyStopping
@@ -14,60 +12,70 @@ from sklearn.model_selection import train_test_split
 import urllib.request
 
 ################################################################################
-#define new architecture for the NN, same as in prep_work.py but with an additional, "summed" layer
-def equivariant_layer(inp, number_of_channels_in, number_of_channels_out):
-    # two parameters:
-    # (1) Multiply every element of the matrix by a parameter
-    # (2) Take the average of all matrix elements, which gives a 1x1 matrix. Repeat that number to get a 12x15 matrix. Multiply the result by a parameter
-    inp = layers.Reshape((5, number_of_channels_in))(inp)
-    # ---(1)---
-    out1 = layers.Conv1D(number_of_channels_out, 1, strides=1, padding='valid', use_bias=False, activation='relu')(inp)
-    # ---(2)---
-    out2 = layers.AveragePooling1D(pool_size=5, strides=1, padding='valid')(inp)
-    repeated = [out2 for _ in range(5)]
-    out2 = layers.Concatenate(axis=1)(repeated)
-    out2 = layers.Conv1D(number_of_channels_out, 1, strides=1, padding='valid', use_bias=True, activation='relu')(out2)
-    # return out1, out2
-    return layers.Add()([out1,out2])
-
-def get_deep_sets_network(pooling='sum'):
-    number_of_channels = 30
-    inp = layers.Input(shape=(5,))
-    inp_list = [inp for _ in range(number_of_channels)]
-    inp_duplicated = layers.Concatenate(axis=1)(inp_list)
-    e1 = equivariant_layer(inp_duplicated, number_of_channels, number_of_channels)
-    e1 = layers.Dropout(0.5)(e1)
-    e2 = equivariant_layer(e1, number_of_channels, number_of_channels)
-    e2 = layers.Dropout(0.5)(e2)
-    e3 = equivariant_layer(e2, number_of_channels, number_of_channels)
-    e3 = layers.Dropout(0.5)(e3)
-    p1 = layers.AveragePooling1D(5, strides=1, padding='valid')(e3)
-    p2 = layers.Flatten()(p1)
-    fc1 = layers.Dense(64, activation='relu')(p2)
-    fc2 = layers.Dense(32, activation='relu')(fc1)
-    out = layers.Dense(1, activation='linear')(fc2)
-
-    model = models.Model(inputs=inp, outputs=out)
-    model.compile(
-        loss='mean_squared_error',
-        optimizer=optimizers.Adam(0.001),
-        metrics=['accuracy'],
-    )
-    return model
+# simple test for confirming permutation invariance.
 
 def permute_vector(vector):
     return np.random.permutation(vector)
 
+def permutation_invariance_confirmation(model, test_inputs):
+    permuted_test_inputs = np.apply_along_axis(permute_vector, 1, test_inputs) 
+    if np.all(np.abs(model.predict(permuted_test_inputs) - model.predict(test_inputs)) < 0.5):
+        print("Model is invariant to permutation of the input vectors.")
+    else:
+        print("Model is not invariant to permutation of the input vectors.")
+
 ################################################################################
-#running the program: 
+# deep sets architecture: essentially training individual NNs in parallel for
+# all the elements in parallel, then aggregating, then training further.
+
+def get_deep_sets_network():
+    inp = tf.keras.layers.Input(shape=(5,))
+    
+    # split the input vector into 5 elements
+    splits = tf.keras.layers.Lambda(lambda x: [x[:, i:i+1] for i in range(5)])(inp)
+    
+    # define shared model which runs in parallel for all 5 elements
+    def get_shared_parallel_model():
+        input_element = tf.keras.layers.Input(shape=(1,))
+        h_a = tf.keras.layers.Dense(32, activation='relu')(input_element)
+        h_b = tf.keras.layers.Dense(64, activation='relu')(h1)
+        output_element = tf.keras.layers.Dense(128, activation='relu')(h2)
+        return tf.keras.models.Model(input_element, output_element)
+    
+    shared_model = get_shared_parallel_model()
+    
+    # apply the shared model to each element
+    parallel_outputs = [shared_model(split) for split in splits]
+    
+    # sum the outputs of these individual models running in parallel
+    parallel_outputs_sum = tf.keras.layers.Lambda(lambda x: tf.reduce_sum(x, axis=0))(parallel_outputs)
+    
+    # further training on the summed output
+    h1 = tf.keras.layers.Dense(64, activation='relu')(parallel_outputs_sum)
+    h2 = tf.keras.layers.Dense(32, activation='relu')(h1)
+    final_output = tf.keras.layers.Dense(1)(h2)
+    
+    # define the overall model
+    model = tf.keras.models.Model(inputs=inp, outputs=final_output)
+    
+    model.compile(
+        loss='mean_squared_error',
+        optimizer=tf.keras.optimizers.Adam(0.001),
+        metrics=['accuracy']
+    )
+    
+    return model
+
+################################################################################
+# running the program: 
 
 if __name__ == '__main__':
-    #training on the sasakain hodge numbers, as in the paper
     X,y = data_wrangle_S()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5) #split data into training and testing
-    model, history = train_network(X_train, y_train, X_test, y_test, get_deep_sets_network()) #train network on chosen data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5) # split data into training and testing
+    model, history = train_network(X_train, y_train, X_test, y_test, get_deep_sets_network()) # train network on chosen data
 
-    permuted_X_test = np.apply_along_axis(permute_vector, 1, X_test)
     print('Accuracy on ordered weights: ' + str(round(daattavya_accuracy(y_train, X_test, y_test, model)*100, 1)) + '%')
     print('Accuracy on randomly permuted weights: ' + str(round(daattavya_accuracy(y_train, permuted_X_test, y_test, model)*100, 1)) + '%')
-    #expect these two accuracies to be similar if the NN is group invariant
+    # expect these two accuracies to be similar if the NN is group invariant
+
+    permutation_invariance_confirmation(model, X_test)
