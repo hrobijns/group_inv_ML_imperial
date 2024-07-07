20e550f9665a54929accaf70546f08f0a1633cc9
Hugo Robijns <155160680+hrobijns@users.noreply.github.com>
2024-07-07 14:52:18 +0100

    Update simple_PINN.py

--- simple_PINN.py
+++ simple_PINN.py
@@ -1,42 +1,36 @@
-# a simple PINN aimed at solving the heat equation, i.e. del(u) / del(t) = alpha * del^2(u) / del(x)^2
+# a simple PINN aimed at solving del^2(u)/del(x)^2 = sin(x)
 ################################################################################
 # import relevant libraries
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 ################################################################################
-def generate_colation_points(x_lim_lower, x_lim_higher, t_lim_lower, t_lim_higher, number_points):
-  # generate colocation points
+# training data generation: colocation points (at which physics loss is calculated) and boundary condition points to enforce agreement with b.c.s
+
+def generate_colocation_points(x_lim_lower, x_lim_higher, number_points):
   x_colocation = np.random.uniform(low=x_lim_lower, high=x_lim_higher, size=(number_points, 1)).astype(np.float32)
-  t_colocation = np.random.uniform(low=t_lim_lower, high=t_lim_higher, size=(number_points, 1)).astype(np.float32)
   x_colocation_tf = tf.convert_to_tensor(x_colocation, dtype=tf.float32)
-  t_colocation_tf = tf.convert_to_tensor(t_colocation, dtype=tf.float32)
-  return x_colocation_tf, t_colocation_tf
+  return x_colocation_tf
 
 def generate_boundary_points():
-    # boundary points for u(x=0.5, t=0)=1
-    x_boundary1 = np.full((1, 1), 0.5).astype(np.float32)
-    t_boundary1 = np.zeros((1, 1)).astype(np.float32)
-    u_boundary1 = np.full((1, 1), 2).astype(np.float32)
+    # boundary points for u(x=0)=0
+    x_boundary1 = np.full((1, 1), 0.0).astype(np.float32)
+    u_boundary1 = np.full((1, 1), 0.0).astype(np.float32)
 
-    # boundary points for u(x=1, t=0)=0
-    x_boundary2 = np.full((1, 1), 1.0).astype(np.float32)
-    t_boundary2 = np.zeros((1, 1)).astype(np.float32)
-    u_boundary2 = np.full((1, 1), 0.0).astype(np.float32)
+    # boundary points for u(x=3)=2.859
+    x_boundary2 = np.full((1, 1), 3.0).astype(np.float32)
+    u_boundary2 = np.full((1, 1), 2.859).astype(np.float32)
 
     # concatenate the boundary points
     x_boundary = np.concatenate([x_boundary1, x_boundary2], axis=0)
-    t_boundary = np.concatenate([t_boundary1, t_boundary2], axis=0)
     u_boundary = np.concatenate([u_boundary1, u_boundary2], axis=0)
 
     # convert to tensors
     x_boundary_tf = tf.convert_to_tensor(x_boundary, dtype=tf.float32)
-    t_boundary_tf = tf.convert_to_tensor(t_boundary, dtype=tf.float32)
     u_boundary_tf = tf.convert_to_tensor(u_boundary, dtype=tf.float32)
 
-    return x_boundary_tf, t_boundary_tf, u_boundary_tf
+    return x_boundary_tf, u_boundary_tf
 
 ################################################################################
 # define PINN
@@ -49,48 +43,42 @@ class PINN(tf.keras.Model):
     self.output_layer = tf.keras.layers.Dense(1, activation=None)
   
   def call(self, inputs): # passes the inputs through the architecture defined above
-    x = inputs[:, 0:1]
-    t = inputs[:, 1:2]
-    concat_input = tf.concat([x, t], axis=1)
-    hidden1 = self.dense1(concat_input)
+    hidden1 = self.dense1(inputs)
     hidden2 = self.dense2(hidden1)
     hidden3 = self.dense3(hidden2)
     output = self.output_layer(hidden3)
     return output
 
-  def compute_gradients(self, x, t): #computes required gradients for minimising loss
+  def compute_gradients(self, x): # computes required gradients for minimising loss
     with tf.GradientTape(persistent=True) as tape:
       tape.watch(x)
-      tape.watch(t)
-      u_pred = self(tf.concat([x, t], axis=1))
+      u_pred = self(x)
       u_x = tape.gradient(u_pred, x)
       u_xx = tape.gradient(u_x, x)
-      u_t = tape.gradient(u_pred, t)
     del tape
-    return u_xx, u_t                  
+    return u_xx
 
 # define loss function
-def loss(model, x_colocation, t_colocation, x_boundary, t_boundary, u_boundary):
+def loss(model, x_colocation, x_boundary, u_boundary):
     # compute loss at colocation points
-    alpha = 0.01
-    u_xx, u_t = model.compute_gradients(x_colocation, t_colocation)
-    pde_loss = u_t - alpha * u_xx
+    u_xx = model.compute_gradients(x_colocation)
+    pde_loss = u_xx - tf.sin(x_colocation)
     physics_loss = tf.reduce_mean(tf.square(pde_loss))
 
     # compute loss at boundary points
-    u_boundary_pred = model(tf.concat([x_boundary, t_boundary], axis=1))
+    u_boundary_pred = model(x_boundary)
     boundary_loss = tf.reduce_mean(tf.square(u_boundary_pred - u_boundary))
 
     return physics_loss + boundary_loss # scale if necessary
 
 ################################################################################
 # define training of the model
-def train_network(x_colocation_tf, t_colocation_tf, x_boundary_tf, t_boundary_tf, u_boundary_tf, model=PINN(), num_epochs=501):
+def train_network(x_colocation_tf, x_boundary_tf, u_boundary_tf, model=PINN(), num_epochs=501):
   optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
   
   for epoch in range(num_epochs):
       with tf.GradientTape() as tape:
-          loss_value = loss(model, x_colocation_tf, t_colocation_tf, x_boundary_tf, t_boundary_tf, u_boundary_tf)
+          loss_value = loss(model, x_colocation_tf, x_boundary_tf, u_boundary_tf)
   
       gradients = tape.gradient(loss_value, model.trainable_variables)
       optimizer.apply_gradients(zip(gradients, model.trainable_variables))
@@ -100,41 +88,28 @@ def train_network(x_colocation_tf, t_colocation_tf, x_boundary_tf, t_boundary_tf
   return model
 
 ################################################################################
-# define animation function which plots graph of u(x) over time - just for visualisation purposes.
-def plot_PINN_prediction(model, x_lim_lower, x_lim_higher, t_lim_lower, t_lim_higher):
-  
-  x_test = np.linspace(x_lim_lower, x_lim_higher, 500).reshape(-1, 1)
-  # create a figure and axis for the animation
-  fig, ax = plt.subplots()
-  line, = ax.plot(x_test, np.zeros_like(x_test))
-  ax.set_xlabel('x')
-  ax.set_ylabel('u')
-  ax.set_title('PINN Prediction of $u(x,t)$')
-  
-  def animate(t):
-      t_test = np.full_like(x_test, t)
-      u_pred = model(tf.concat([x_test, t_test], axis=1)).numpy()
-      line.set_ydata(u_pred)
-      ax.set_title(f'PINN approximation of $u(x)$ at t={t:.2f}')
-      return line,
+# visualisation of the solution
+def analytical_solution(x):
+  return x - np.sin(x) 
 
-  #to-do change 
-  ax.set_xlim(x_lim_lower, x_lim_higher)
-  ax.set_ylim(0, 5)
-  
-  # create the animation
-  t_values = np.linspace(t_lim_lower, t_lim_higher, 400)
-  ani = animation.FuncAnimation(fig, animate, frames=t_values, interval=20, blit=True)
-  # save the animation as an MP4 file
-  ani.save('pinn_prediction_animation.mp4', writer='ffmpeg')
-  # display the animation
+def plot_PINN_prediction(x_lim_lower, x_lim_higher, model):
+  x_test = np.linspace(x_lim_lower, x_lim_higher, 300).reshape(-1, 1).astype(np.float32)
+  x_test_tf = tf.convert_to_tensor(x_test, dtype=tf.float32)
+  u_pred = model(x_test_tf)
+  plt.plot(x_test, analytical_solution(x_test), linestyle = ":", label='analytical')
+  plt.plot(x_test, u_pred.numpy(), alpha=0.5, label='PINN predicted solution')
+  plt.scatter(x_boundary_tf.numpy(), u_boundary_tf.numpy(), color='red', s = 8, label='b.c. points')
+  plt.xlim(x_lim_lower,x_lim_higher)
+  plt.xlabel('x')
+  plt.ylabel('u')
+  plt.legend()
   plt.show()
 
 ################################################################################
 # running the program:
 if __name__ == '__main__':
-  x_colocation_tf, t_colocation_tf = generate_colation_points(x_lim_lower=0, x_lim_higher=1, t_lim_lower=0, t_lim_higher=30, number_points=1000)
-  x_boundary_tf, t_boundary_tf, u_boundary_tf = generate_boundary_points()
-  model = train_network(x_colocation_tf, t_colocation_tf, x_boundary_tf, t_boundary_tf, u_boundary_tf)
+  x_colocation_tf = generate_colocation_points(0, 5, 1000)
+  x_boundary_tf, u_boundary_tf = generate_boundary_points()
+  trained_model = train_network(x_colocation_tf, x_boundary_tf, u_boundary_tf, PINN())
+  plot_PINN_prediction(0, 5, trained_model)
 
-  plot_PINN_prediction(model, x_lim_lower=0, x_lim_higher=1, t_lim_lower=0, t_lim_higher=50)
